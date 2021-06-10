80ed2191831e3dfb10a89037ff7da2c4f649cba0
hansendo <hansendo.d3s@gmail.com>
2021-06-10 23:05:30 +0900

    Webアプリを作ります

--- svelte-rest-app/src/routes/index.svelte
+++ svelte-rest-app/src/routes/index.svelte
@@ -1,2 +1,100 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+import { beforeUpdate } from 'svelte';
+
+	let name = 'name';
+	let lv = 10;
+	let max_hp = 1000;
+	let hp = 80;
+	let max_mp = 10;
+	let mp = 8;
+	let agi = 5;
+	let tec = 2;
+	let str = 9;
+	let luc = 1;
+	let text = `I'm stronger.`;
+
+	beforeUpdate(()=>{
+		max_hp = lv * str;
+		max_mp = Math.round((lv * agi) / 2);
+	});
+</script>
+
+<div class="status-view">
+	<div class="status-image">
+		{name}<br>
+		<img src="https://uploads.codesandbox.io/uploads/user/5f10fa6b-67dd-441c-bf2d-03b8f3dc3846/qsNQ-alienYellow.png" alt="character" />
+	</div>
+	<div class="status-data">
+		Lv: {lv}<br>
+		HP: {hp}/ {max_hp}<br>
+		MP: {mp}/ {max_mp}<br>
+		<br>
+		AGI: {agi}  STR: {str}<br>
+		TEC: {tec}  LUC: {luc}
+	</div>
+	<div class="status-text">
+		{text}
+	</div>
+</div>
+
+<div class="status-config">
+	config<br>
+	name : <input type="text" maxlength="16" bind:value="{name}" />
+	<br>
+	Lv : <input type="number" min="1" max="99" bind:value="{lv}" />
+	<br>
+	hp : <input type="number" min="0" max="{max_hp}" bind:value={hp} />
+	mp : <input type="number" min="0" max="{max_mp}" bind:value={mp} />
+	<br>
+	AGI : <input type="number" min="1" max="99" bind:value={agi} />
+	STR : <input type="number" min="1" max="99" bind:value={str} />
+	<br>
+	TEC : <input type="number" min="1" max="99" bind:value={tec} />
+	LUC : <input type="number" min="1" max="99" bind:value={luc} />
+	<br>
+	text : <br>
+	<textarea bind:value="{text}"></textarea>
+</div>
+
+<style>
+.status-view {
+	width: 320px;
+	height: 240px;
+	margin: 2em auto;
+	padding: 1em;
+	background-color: blue;
+	color: white;
+	display: flex;
+	flex-wrap: wrap;
+	text-shadow: 1px 1px black;
+	font-family: monospace;
+	font-size: 16px;
+}
+.status-image {
+	width: 128px;
+}
+.status-data {
+	flex-grow: 1;
+	padding-left: 1em;
+}
+.status-text {
+	width: 100%;
+	padding: 0.5em;
+	border: 2px ridge white;
+	overflow: hidden;
+}
+img {
+	display: block;
+	margin: auto;
+}
+.status-config {
+	width: 320px;
+	margin: 1em auto 2em;
+	padding: 1em;
+	border: 1px solid black;
+	line-height: 2em;
+}
+.status-config input[type=number] {
+	max-width: 6em;
+}
+</style>
